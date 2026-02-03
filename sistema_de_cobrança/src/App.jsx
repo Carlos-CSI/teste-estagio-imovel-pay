6441a7a8beb135c2ee158eef7737dd0d11497f1a
Yasmim Petri <yasmimpetri7@gmail.com>
2026-02-03 17:23:48 -0300

    estrutura inicial do frontend

---
+++ sistema_de_cobrança/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div>
+        <h1>Bem vindo ao sistema de cobranças</h1>
+    <button type="button">Lista de cobranças</button>
+    <button type="button">Nova cobrança</button>
+    </div>
+    </>
+  )
+}
+
+export default App
