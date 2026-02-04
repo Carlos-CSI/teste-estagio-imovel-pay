7bfd1194bffdf05f3eea2b2de7041b3b43dfa85a
Amos Barbato <amosbarbato@gmail.com>
2026-02-04 11:46:34 -0300

    feat: add header component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,9 @@
+import Header from "./components/header";
+
 export default function App() {
   return (
-    <div>
-      <header>
-        <h1 className="bg-white text-red-600">Hello World</h1>
-      </header>
-    </div>
+    <>
+      <Header />
+    </>
   );
 }
---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,12 @@
+export default function Header() {
+  return (
+    <header className="gradient py-10 text-white shadow-lg">
+      <div className="mx-auto max-w-7xl space-y-2.5">
+        <h1 className="text-4xl font-bold">💰 Sistema de Cobranças</h1>
+        <p className="text-lg opacity-90">
+          Gerencie suas cobranças de forma simples e eficiente
+        </p>
+      </div>
+    </header>
+  );
+}
