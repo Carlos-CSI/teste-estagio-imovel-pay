92efe24f4d1e98b7b5d3c9f20113575bf41bdf59
Amos Barbato <amosbarbato@gmail.com>
2026-02-04 14:19:44 -0300

    feat: feat: add debtList component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,25 @@
 import Header from "./components/header";
+import { Button } from "./components/ui/button";
+import DebtList from "./components/debtList";
+import { Plus } from "lucide-react";
 
 export default function App() {
   return (
     <>
       <Header />
+
+      <main className="flex-1 py-10">
+        <div className="mx-auto max-w-7xl space-y-8">
+          <div className="flex justify-end">
+            <Button>
+              <Plus className="size-4.5" />
+              Criar cobrança
+            </Button>
+          </div>
+
+          <DebtList />
+        </div>
+      </main>
     </>
   );
 }
---
+++ frontend/src/components/debtList.tsx
@@ -0,0 +1,47 @@
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./ui/select";
+import DebtItem from "./debtItem";
+
+export default function DebtList() {
+  return (
+    <section className="space-y-6 rounded-lg bg-white p-8 shadow-md">
+      <div className="flex flex-wrap items-center justify-between gap-4">
+        <h2 className="text-2xl font-bold">Lista de Cobranças</h2>
+
+        {/* Filtros de pesquisa */}
+        <Select>
+          <SelectTrigger className="w-60">
+            <SelectValue placeholder="Filtrar por status:" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectGroup>
+              <SelectItem value="TODOS">Todos</SelectItem>
+              <SelectItem value="PENDENTE">Pendentes</SelectItem>
+              <SelectItem value="PAGO">Pagos</SelectItem>
+            </SelectGroup>
+          </SelectContent>
+        </Select>
+      </div>
+
+      {/* Listagem de cobranças */}
+      <div className="">
+        <div className="mb-4 border-b border-gray-300 pb-2.5">
+          <span className="text-sm font-medium text-gray-600">
+            2 cobranças encontrada(s)
+          </span>
+        </div>
+        <div className="space-y-4">
+          <DebtItem />
+          <DebtItem />
+          <DebtItem />
+        </div>
+      </div>
+    </section>
+  );
+}
