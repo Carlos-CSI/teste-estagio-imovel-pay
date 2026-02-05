34fa2f1418d6441ee621d8ab70cf9cf4734515a7
Amos Barbato <amosbarbato@gmail.com>
2026-02-05 15:26:03 -0300

    feat: implements communication with the API

---
+++ frontend/src/services/api.ts
@@ -0,0 +1,45 @@
+import { DebtStatus, type ApiResponse, type Debt } from "../types";
+
+const API_URL = import.meta.env.VITE_API_URL || "http://localhost:3001";
+
+// Função para fazer requisições HTTP
+const request = async <T>(
+  endpoint: string,
+  options: RequestInit = {},
+): Promise<ApiResponse<T>> => {
+  const url = `${API_URL}${endpoint}`;
+
+  const config: RequestInit = {
+    headers: {
+      "Content-Type": "application/json",
+    },
+    ...options,
+  };
+
+  try {
+    const response = await fetch(url, config);
+    const data: ApiResponse<T> = await response.json();
+
+    if (!response.ok) {
+      throw new Error(data.errors?.join(", ") || "Erro desconhecido");
+    }
+
+    return data;
+  } catch (error) {
+    if (error instanceof Error) {
+      throw Error;
+    }
+    throw new Error("Erro desconhecido na requisição");
+  }
+};
+
+// Api de cobranças
+export const debtAPI = {
+  // Lista todas as cobranças
+  toListAll: async (
+    status: DebtStatus | null = null,
+  ): Promise<ApiResponse<Debt[]>> => {
+    const query = status ? `?status=${status}` : "";
+    return request<Debt[]>(`/api/cobrancas${query}`);
+  },
+};
